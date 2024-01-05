961da1370ca174e7f02c597c88480ad9f78bb929
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-01-05 16:34:56 +0100

    updating uploads helper

--- src/Helpers/UtilsI18nHelper.php
+++ src/Helpers/UtilsI18nHelper.php
@@ -10,8 +10,6 @@ declare(strict_types=1);
 
 namespace EightshiftFormsUtils\Helpers;
 
-use EightshiftFormsUtils\Config\UtilsConfig;
-
 /**
  * Class UtilsI18nHelper
  */
@@ -24,26 +22,18 @@ final class UtilsI18nHelper
 	 */
 	public static function getLocale(): string
 	{
-		$locale = '';
-		$localeInit = '';
+		$output = '';
 
 		$filterName = UtilsGeneralHelper::getFilterName(['general', 'locale']);
 		if (\has_filter($filterName)) {
-			$locale = \apply_filters($filterName, $localeInit);
+			$locale = \apply_filters($filterName, []);
+
+			$defaultLanguage = $locale['default'] ?? '';
+			$currentLanguage = $locale['current'] ?? '';
+
+			return $defaultLanguage === $currentLanguage ? $defaultLanguage : $currentLanguage;
 		}
 
-		$useWpml = \apply_filters(UtilsConfig::FILTER_SETTINGS_WPML_IS_VALID_NAME, []);
-		if ($useWpml) {
-			$defaultLanguage = \apply_filters('wpml_default_language', null);
-			$currentLanguage = \apply_filters('wpml_current_language', null);
-
-			if ($defaultLanguage === $currentLanguage) {
-				$locale = $localeInit;
-			} else {
-				$locale = $currentLanguage;
-			}
-		}
-
-		return $locale;
+		return $output;
 	}
 }

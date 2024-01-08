8a382fd9f9942de52b8a9344f6ac96490186109b
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-01-08 16:28:55 +0100

    updating uploads helper

--- src/Helpers/UtilsHooksHelper.php
+++ src/Helpers/UtilsHooksHelper.php
@@ -26,36 +26,7 @@ final class UtilsHooksHelper
 	 */
 	public static function getFilterName(array $names): string
 	{
-		$filters = \wp_cache_get(UtilsConfig::FILTER_PREFIX . '_filters_public_list', UtilsConfig::FILTER_PREFIX);
-
-		// Cache filter names for faster access.
-		if (!$filters) {
-			$filters = self::getAllPublicFiltersNames(\apply_filters(UtilsConfig::FILTER_PUBLIC_FILTERS_DATA, []));
-
-			\wp_cache_add(UtilsConfig::FILTER_PREFIX . '_filters_public_list', $filters, UtilsConfig::FILTER_PREFIX, \HOUR_IN_SECONDS);
-		}
-
-		// List of all keys provided for the filter name.
-		$names = \array_map(
-			function ($item) {
-				return UtilsGeneralHelper::kebabToSnakeCase(UtilsGeneralHelper::camelToSnakeCase($item));
-			},
-			$names
-		);
-
-		// Create a string from array.
-		$names = \implode('_', $names);
-
-		// Create a full filter name.
-		$filterName = UtilsConfig::FILTER_PREFIX . "_{$names}";
-
-		if (!\in_array($filterName, $filters, true)) {
-			// translators: %s is the filter name.
-			\trigger_error(\sprintf(\esc_html__("You are using `%s` filter that doesn't exist. Please check the documentation for the correct filter name!", 'eightshift-forms'), \esc_attr($filterName)), \E_USER_WARNING); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
-			return '';
-		}
-
-		return $filterName;
+		return self::getHookName($names, 'filters', 'filter', \apply_filters(UtilsConfig::FILTER_PUBLIC_FILTERS_DATA, []));
 	}
 
 	/**
@@ -67,13 +38,28 @@ final class UtilsHooksHelper
 	 */
 	public static function getActionName(array $names): string
 	{
-		$actions = \wp_cache_get(UtilsConfig::FILTER_PREFIX . '_actions_public_list', UtilsConfig::FILTER_PREFIX);
+		return self::getHookName($names, 'actions', 'action', \apply_filters(UtilsConfig::FILTER_PUBLIC_ACTIONS_DATA, []));
+	}
+
+	/**
+	 * Build a full filter name from array of names.
+	 *
+	 * @param array<int, string> $names Array of names.
+	 * @param string $cacheName Cache name.
+	 * @param string $label Label.
+	 * @param array<mixed> $dataSet Data set.
+	 *
+	 * @return string
+	 */
+	private static function getHookName(array $names, string $cacheName, string $label, array $dataSet): string
+	{
+		$output = \wp_cache_get(UtilsConfig::FILTER_PREFIX . "_{$cacheName}_public_list", UtilsConfig::FILTER_PREFIX);
 
 		// Cache filter names for faster access.
-		if (!$actions) {
-			$actions = self::getAllPublicFiltersNames(\apply_filters(UtilsConfig::FILTER_PUBLIC_ACTIONS_DATA, []));
+		if (!$output) {
+			$output = self::getHooksList($dataSet);
 
-			\wp_cache_add(UtilsConfig::FILTER_PREFIX . '_actions_public_list', $actions, UtilsConfig::FILTER_PREFIX, \HOUR_IN_SECONDS);
+			\wp_cache_add(UtilsConfig::FILTER_PREFIX . "_{$cacheName}_public_list", $output, UtilsConfig::FILTER_PREFIX, \HOUR_IN_SECONDS);
 		}
 
 		// List of all keys provided for the filter name.
@@ -88,15 +74,15 @@ final class UtilsHooksHelper
 		$names = \implode('_', $names);
 
 		// Create a full filter name.
-		$actionName = UtilsConfig::FILTER_PREFIX . "_{$names}";
+		$outputName = UtilsConfig::FILTER_PREFIX . "_{$names}";
 
-		if (!\in_array($actionName, $actions, true)) {
+		if (!\in_array($outputName, $output, true)) {
 			// translators: %s is the filter name.
-			\trigger_error(\sprintf(\esc_html__("You are using `%s` action that doesn't exist. Please check the documentation for the correct action name!", 'eightshift-forms'), \esc_attr($actionName)), \E_USER_WARNING); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+			\trigger_error(\sprintf(\esc_html__('You are using `%1$s` %2$s that doesn\'t exist. Please check the documentation for the correct action name!', 'eightshift-forms'), \esc_attr($outputName), \esc_attr($label)), \E_USER_WARNING); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
 			return '';
 		}
 
-		return $actionName;
+		return $outputName;
 	}
 
 	/**
@@ -107,13 +93,13 @@ final class UtilsHooksHelper
 	 *
 	 * @return array<int, string>
 	 */
-	private static function getAllPublicFiltersNames(array $data, string $prefix = ''): array
+	private static function getHooksList(array $data, string $prefix = ''): array
 	{
 		$output = [];
 
 		foreach ($data as $key => $value) {
 			if (\is_array($value)) {
-				$nestedKeys = self::getAllPublicFiltersNames($value, $prefix . UtilsGeneralHelper::kebabToSnakeCase(UtilsGeneralHelper::camelToSnakeCase($key)) . '_');
+				$nestedKeys = self::getHooksList($value, $prefix . UtilsGeneralHelper::kebabToSnakeCase(UtilsGeneralHelper::camelToSnakeCase($key)) . '_');
 				$output = \array_merge($output, $nestedKeys);
 			} else {
 				$output[] = UtilsConfig::FILTER_PREFIX . '_' . $prefix . UtilsGeneralHelper::kebabToSnakeCase(UtilsGeneralHelper::camelToSnakeCase($value));

094a8ddc0bc5fdbdda3aab6e8d9bb330b6eb1644
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-01-12 09:19:35 +0100

    updating uploads helper

--- src/Rest/Routes/AbstractUtilsBaseRoute.php
+++ src/Rest/Routes/AbstractUtilsBaseRoute.php
@@ -67,7 +67,14 @@ abstract class AbstractUtilsBaseRoute extends AbstractRoute implements CallableR
 	 *
 	 * @return array<string, mixed> Either an array of options for the endpoint, or an array of arrays for multiple methods.
 	 */
-	abstract protected function getCallbackArguments(): array;
+	protected function getCallbackArguments(): array
+	{
+		return [
+			'methods' => $this->getMethods(),
+			'callback' => [$this, 'routeCallback'],
+			'permission_callback' => [$this, 'permissionCallback'],
+		];
+	}
 
 	/**
 	 * Returns allowed methods for this route.

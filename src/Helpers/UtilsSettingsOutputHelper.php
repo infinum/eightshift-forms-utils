836e1118704d5b7c245d76bc640fc9b409be2301
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-06-04 17:49:32 +0200

    adding additional options for caching

--- src/Helpers/UtilsSettingsOutputHelper.php
+++ src/Helpers/UtilsSettingsOutputHelper.php
@@ -117,7 +117,7 @@ final class UtilsSettingsOutputHelper
 				[
 					'component' => 'intro',
 					'introTitle' => \__('Disclaimer', 'eightshift-forms'),
-					'introSubtitle' => \sprintf(\__("Eightshift Forms doesn't configure the %s app or any other third-party tools. However, enabling this feature adds necessary configurations in the backend for everything to function correctly.", 'eightshift-forms'), esc_html($type)),
+					'introSubtitle' => \sprintf(\__("Eightshift Forms doesn't configure the %s or any other third-party tools. However, enabling this feature adds necessary configurations in the backend for everything to function correctly.", 'eightshift-forms'), esc_html($type)),
 				],
 			],
 		];
